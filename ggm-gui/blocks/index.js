0b305f58f29839ac5133b270fa9b718e290fd7f7
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-06-26 15:28:31 -0400

    Upload From Shell Script

---
+++ ggm-gui/blocks/index.js
@@ -0,0 +1,29 @@
+
+//load blocks
+gui.loadScript("blocks/control.js",function () {
+	gui.loadScript("blocks/operators.js",function () {
+		gui.loadScript("blocks/files.js",function () {
+			gui.loadScript("blocks/game.js",function () {
+				gui.loadScript("blocks/audio.js",function () {
+					gui.loadScript("blocks/lists.js",function () {
+						gui.loadScript("blocks/loging.js",function () {
+							gui.loadScript("blocks/other.js",function () {
+								gui.loadScript("blocks/variables.js",function () { //aka "display"
+									loadDefaultGame(); //load function, for modders out there, call this when making a new game.
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+});
+
+
+function myUpdateFunction(event) {
+  var code = Blockly.JavaScript.workspaceToCode(workspace);
+  vm.code = code;
+}
+workspace.addChangeListener(myUpdateFunction);
+console.log("[gui]:blocks loaded.");
